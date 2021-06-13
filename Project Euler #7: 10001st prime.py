f731f1d04d16266dcf501c6ebd4f1c4bc2fa8bd0
Ananda Wiradharma <anandawira754@gmail.com>
2021-06-13 21:24:58 +0700

    euler 7

---
+++ Project Euler #7: 10001st prime.py
@@ -0,0 +1,36 @@
+#!/bin/python3
+
+def SieveOfEratosthenes(n):
+ 
+    # Create a boolean array
+    # "prime[0..n]" and initialize
+    #  all entries it as true.
+    # A value in prime[i] will
+    # finally be false if i is
+    # Not a prime, else true.
+    prime = [True for i in range(n+1)]
+    p = 2
+    while (p * p <= n):
+ 
+        # If prime[p] is not
+        # changed, then it is a prime
+        if (prime[p] == True):
+ 
+            # Update all multiples of p
+            for i in range(p * p, n+1, p):
+                prime[i] = False
+        p += 1
+        
+    # Generate the list
+    primeList = [idx for idx, val in enumerate(prime) if val]
+    return primeList
+
+primeList = SieveOfEratosthenes(105000)
+
+def solve(n):
+    print(primeList[n+1])
+
+t = int(input().strip())
+for a0 in range(t):
+    n = int(input().strip())
+    solve(n)
